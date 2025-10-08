5b0faaa1b860ca875285410e902febd9c76dc9a7
Mikkel Riise Lund <mrl@cvation.com>
2025-10-08 16:55:24 +0200

    Adds logo.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import logoUrl from '$lib/assets/logo.png?url';
+</script>
+
+<section class="hero">
+	<img src={logoUrl} alt="Milestone marker logo" width="200" />
+	<div class="hero__copy">
+		<h1>Milestone Marker</h1>
+		<p>
+			<b>Never miss a milestone again.</b> Get memorable dates &dash; like when you turn 1234 weeks,
+			1 billion seconds, or 1/3 of 100 years &dash; added directly to your calendar. Pick your birth
+			date and time zone to get a custom link.
+		</p>
+	</div>
+</section>
