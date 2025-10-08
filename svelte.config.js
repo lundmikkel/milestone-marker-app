b46e2f609b06f4af3e458b69dd5156c8cb24e500
Mikkel Riise Lund <mrl@cvation.com>
2025-10-08 16:37:26 +0200

    Adds deploy workflow.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,22 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+const base = dev ? '' : process.env.BASE_PATH ?? '';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base
+		}
+	}
 };
 
 export default config;
